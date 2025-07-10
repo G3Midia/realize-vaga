8a465e9ce2cb3279c3da60095a3d1dac971305c6
G3Midia <granval.dc@gmail.com>
2025-07-10 12:14:44 -0400

    Update apideconversao.js

--- apideconversao.js
+++ apideconversao.js
@@ -5,7 +5,7 @@ const data = JSON.stringify({
   event_name: 'Lead',
   event_time: Math.floor(new Date() / 1000),
   action_source: 'website',
-  event_source_url: 'http://g3midia.github.io/sunflower.html/',
+  event_source_url: 'http://g3midia.github.io/realize-vaga/',
   user_data: {
     em: 'HASHED_EMAIL', // Email do usuário com hash SHA-256
     ph: 'HASHED_PHONE'  // Telefone do usuário com hash SHA-256
@@ -18,7 +18,7 @@ const data = JSON.stringify({
 
 const options = {
   hostname: 'graph.facebook.com',
-  path: `/v11.0/PIXEL_ID/events?access_token=ACESS_TOKEN`, // Substitua 'SEU_PIXEL_ID' e 'SEU_ACCESS_TOKEN'
+  path: `/v11.0/1416162726361479/events?access_token=EAAMHvwm4ftwBPG022aB55fgGbND7ptZC87Hv1NeZAPCIlm2a7R0NlNQUAW6jW3WBeTxxz08dYfOkRWULZBG7aZBCueGPJpMmsHsdAOamd7t9PNVXgF1ZCluUohkUjlDizaZBXoDZBgrW72a6cnFFNHnMM34IOppTpM0SXs3RQ0ZAgmffZBhFx5vKMxxL9phtrmFpyrAZDZD`, // Substitua 'SEU_PIXEL_ID' e 'SEU_ACCESS_TOKEN'
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
